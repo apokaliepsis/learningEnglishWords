fb3d4b936e632fe1cc8ee2cf8c8e2dcb9ab48db4
anton <antonsalekseev@gmail.com>
2022-09-07 10:03:09 +0700

    Добавлено словарь топ 2000 слов; Добавлен интервал 1 час

--- src/test/java/com/telegrambot/AppTest.java
+++ src/test/java/com/telegrambot/AppTest.java
@@ -4,8 +4,13 @@ import com.offbytwo.jenkins.model.TestCase;
 import com.telegrambot.audio.Audio;
 import com.telegrambot.database.Database;
 import org.apache.commons.lang.math.RandomUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Time;
 import java.util.*;
@@ -21,7 +26,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AppTest  {
 
     @org.junit.jupiter.api.Test
-    public void fastTest(){
+    void fastTest(){
+
+        Document doc;
+        try {
+            doc = Jsoup.connect("https://translate.google.com/?hl=ru&sl=en&tl=ru&text=she&op=translate").get();
+            //System.out.println(doc);
+            for (Element result : doc.select("html-blob")) {
+                final String text = ((TextNode) result.childNode(0)).getWholeText();
+                System.out.println(text);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
 
     }
